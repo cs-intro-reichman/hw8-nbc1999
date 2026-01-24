f983ddfcc368836b01cce97967c9837f54b0b469
Noam Benhamou <noam.benhamou@post.runi.ac.il>
2026-01-24 21:17:04 +0200

    Completed assignment nbc

--- Network.java
+++ Network.java
@@ -30,9 +30,9 @@ public class Network {
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
-       if (name == null) return null; 
+       if (name == null) return null;
     for (int i = 0; i < userCount; i++) {
-        if (name.equalsIgnoreCase(users[i].getName())) { 
+        if (name.equals(users[i].getName())) { 
             return users[i];
         }
     }
